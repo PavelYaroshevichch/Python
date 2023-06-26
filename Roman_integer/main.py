6070f4e8dd75eee54d21e6f16935ac0e44062224
Pavel <99549788+PavelYaroshevichch@users.noreply.github.com>
2023-06-26 22:54:34 +0300

    Add files via upload

---
+++ Roman_integer/main.py
@@ -0,0 +1,13 @@
+def r_i(s):
+    suma = 0
+    prev = 0
+    res = ''
+    for i in s:
+        if i % 2 == 0:
+            prev = i
+            continue
+        else:
+            res = prev + 
+
+
+
